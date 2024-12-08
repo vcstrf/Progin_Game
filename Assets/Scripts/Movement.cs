39cc72ea970e60018a5338aba27811702c6e5e41
vcstrf_n <filin_18090@mail.ru>
2024-12-08 17:38:14 +1000

    everything fixed & dash added

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -12,12 +12,20 @@ public class Movement : MonoBehaviour
     public bool isCrouching;
     public bool isSprinting;
 
+    [SerializeField] private bool canDash = true;
+    [SerializeField] private bool isDashing;
+    [SerializeField] private float dashingPower = 40f;
+    [SerializeField] private float dashingTime = 0.2f;
+    [SerializeField] private float dashingCooldown = 0.7f;
+
     [SerializeField] Collider2D standingCollider;
     [SerializeField] Transform overheadCheckCollider;
     [SerializeField] bool StartFacingLeft;
     [SerializeField] bool CutsceneNeeded;
     [SerializeField] float overheadCheckRadius = 0.2f;
-    [SerializeField] private bool canDoubleJump;
+    [SerializeField] private int airJumpCounter = 0;
+    [SerializeField] private int maxAirJumps = 1;
+    [SerializeField] private TrailRenderer trail;
 
     public Rigidbody2D body;
     public Collider2D groundCheck;
@@ -40,31 +48,39 @@ public class Movement : MonoBehaviour
         }
     }
 
+    void Update()
+    {
+        GetInput();
+        CheckGround();
+        MoveWithInput();
+        CheckCrouchState();
+    }
+
     void Jump()
     {
         body.velocity = new Vector2(body.velocity.x, jumpSpeed);
     }
 
-    void MoveWithInput()
+    void GetInput()
     {
         xInput = Input.GetAxis("Horizontal");
 
-        if (isGrounded)
+        if (isDashing)
         {
-            canDoubleJump = true;
+            return;
         }
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
             if (isGrounded)
             {
-                canDoubleJump = true;
                 Jump();
+                airJumpCounter = 0;
             }
-            else if (canDoubleJump)
+            else if (airJumpCounter < maxAirJumps)
             {
-                canDoubleJump = false;
                 Jump();
+                airJumpCounter++;
             }
         }
 
@@ -87,18 +103,29 @@ public class Movement : MonoBehaviour
             isSprinting = false;
         }
 
+        if (Input.GetMouseButton(1) && canDash && Mathf.Abs(body.velocity.x) > 1.5f)
+        {
+            StartCoroutine(Dash());
+        }
+        Debug.Log(body.velocity.x);
+
+    }
+
+    void MoveWithInput()
+    {
         if (isGrounded && (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W)))
         {
             body.velocity = new Vector2(body.velocity.x, jumpSpeed);
             isGrounded = false;
         }
 
-        float xVal = xInput * walkSpeed * 100 * Time.deltaTime;
+        float xVal = xInput * walkSpeed;
 
         if (isSprinting)
         {
             xVal *= sprintSpeedModifier;
         }
+
         if (isCrouching)
         {
             xVal *= crouchSpeedModifier;
@@ -106,19 +133,6 @@ public class Movement : MonoBehaviour
 
         body.velocity = new Vector2(xVal, body.velocity.y);
 
-        Flip();
-    }
-
-    void CheckGround()
-    {
-        isGrounded = Physics2D.OverlapAreaAll(groundCheck.bounds.min, groundCheck.bounds.max, groundMask).Length > 0;
-
-        ApplyFriction();
-
-    }
-
-    void Flip()
-    {
         if (facingRight && xInput < 0)
         {
             transform.rotation = Quaternion.Euler(0, 180, 0);
@@ -129,6 +143,18 @@ public class Movement : MonoBehaviour
             transform.rotation = Quaternion.Euler(0, 0, 0);
             facingRight = true;
         }
+    }
+
+    void CheckGround()
+    {
+        isGrounded = Physics2D.OverlapAreaAll(groundCheck.bounds.min, groundCheck.bounds.max, groundMask).Length > 0;
+
+        if (isGrounded)
+        {
+            airJumpCounter = 0;
+        }
+
+        ApplyFriction();
     }
 
     void ApplyFriction()
@@ -161,10 +187,19 @@ public class Movement : MonoBehaviour
         }
     }
 
-    void FixedUpdate()
+    private IEnumerator Dash()
     {
-        CheckGround();
-        MoveWithInput();
-        CheckCrouchState();
+        canDash = false;
+        isDashing = true;
+        float originalGravity = body.gravityScale;
+        body.gravityScale = 0f;
+        body.velocity = new Vector2(transform.localScale.x * dashingPower, 0f);
+        trail.emitting = true;
+        yield return new WaitForSeconds(dashingTime);
+        trail.emitting = false;
+        body.gravityScale = originalGravity;
+        isDashing = false;
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
     }
 }
